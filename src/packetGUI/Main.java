1a6e8ef9ccb0aa7b90de58667ba7e70a3e8cb18f
Jarosław Skrzypczak <jarek.skrzypczak@o2.pl>
2017-01-14 12:00:41 +0100

    Restauracja dodaje się już na mapie. W klasie mapa dodałem macierz przycisków przez które mam bezpośredni dostęp do mapy na view.

--- src/packetGUI/Main.java
+++ src/packetGUI/Main.java
@@ -8,27 +8,135 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
-import javafx.geometry.VPos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import sun.plugin.javascript.navig.Anchor;
+import skeletor.*;
+import skeletor.Food.Meal;
+import skeletor.Person.Client;
+import skeletor.Person.Deliverer;
+import skeletor.Transport.Vehicle;
 
-import java.io.IOException;
-import java.util.Observable;
+import java.util.LinkedList;
+import java.util.Random;
 
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
+    //sekcje krytyczne
+    private static volatile int orderNumber = 1;
 
+    private static volatile Map map;
+    private static volatile LinkedList<DinnerKit> menu = new LinkedList<>();
+    private static volatile LinkedList<Order> orderLinkedList = new LinkedList<>();
+    private static volatile LinkedList<Thread> threads = new LinkedList<>();
+    private static volatile LinkedList<Vehicle> vehicles = new LinkedList<>();
+    private static volatile LinkedList<Client> clients_list = new LinkedList<>();
+
+    //zmienne regulujące
+    private static int width = 20, lenght = 20, vehicleNumber = 20;
+    private static int lRestaurant, wRestaurant;
+    private static RandomGenerator randomGenerator = new RandomGenerator();
+
+    public static Map getMap() {
+        return map;
+    }
+
+    public static LinkedList<Client> getClients_list() {
+        return clients_list;
+    }
+
+    public static void setClients_list(LinkedList<Client> clients_list) {
+        Main.clients_list = clients_list;
+    }
+
+    public static int getlRestaurant() {
+        return lRestaurant;
+    }
+
+    public static void setRestaurantPosition() {
+        Random random = new Random(System.nanoTime());
+        Main.wRestaurant = random.nextInt(getWidth());
+        Main.lRestaurant = random.nextInt(getLenght());
+    }
+
+    public static int getwRestaurant() {
+        return wRestaurant;
+    }
+
+    public static int getVehicleNumber() {
+        return vehicleNumber;
+    }
+
+    public static int getWidth() {
+        return width;
+    }
+
+    public static int getLenght() {
+        return lenght;
+    }
+
+    synchronized
+    public static LinkedList<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public static void setVehicles(LinkedList<Vehicle> vehicles) {
+        Main.vehicles = vehicles;
+    }
+
+    synchronized
+    public static void clearThreadListFromEndProcess() {
+        for (int i = 0; i < threads.size(); i++) {
+            if (!threads.get(i).isAlive()) {
+                threads.remove(i);
+            }
+        }
+    }
+
+    synchronized
+    public static int getOrderNumber() {
+        return orderNumber++;
+    }
+
+    synchronized
+    public static void addOrderToList(Order order) {
+        orderLinkedList.addLast(order);
+    }
+
+    synchronized
+    public static LinkedList<DinnerKit> getMenu() {
+        return menu;
+    }
+
+    public static void setMenu(LinkedList<DinnerKit> menu) {
+        Main.menu = menu;
+    }
+
+    synchronized
+    public static LinkedList<Order> getOrderLinkedList() {
+        return orderLinkedList;
+    }
+
+    public static void setOrderLinkedList(LinkedList<Order> orderLinkedList) {
+        Main.orderLinkedList = orderLinkedList;
+    }
+
+    public static LinkedList<Thread> getThreads() {
+        return threads;
+    }
+
+    public static void setThreads(LinkedList<Thread> threads) {
+        Main.threads = threads;
+    }
+
+    //**********************************************************************************************
+    @Override
+    public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(this.getClass().getResource("sample.fxml"));
 
@@ -46,25 +154,31 @@ public class Main extends Application {
 //        System.out.println("dzieci grida głównego "+children.toString());
         AnchorPane anchorPane_child_scene = (AnchorPane) children.get(0);
         children = anchorPane_child_scene.getChildren();
-//        System.out.println("dzieci anchora " + children.toString());
+        System.out.println("dzieci anchora " + children.toString());
         GridPane gridPaneMap = (GridPane) children.get(12); //wyciągnięcie mapy z view
+        System.out.println(children.size());
+        Label coordinateLabel = (Label) children.get(14);
 //        System.out.println("gridpanelMap "+gridPaneMap.toString());
         gridPaneMap.setAlignment(Pos.CENTER);
         //25 rzędów | 30 kolumn
-        Button[][] mapsButtons = new Button[20][20];
-        for (int i = 0; i< 20; i++){
-            for (int k = 0; k<20; k++){
+
+        Button[][] mapsButtons = new Button[width][lenght];
+
+//USTAWIENIE MAPY WEJSCIOWEJ
+        for (int i = 0; i < width; i++) {
+            for (int k = 0; k < lenght; k++) {
                 mapsButtons[i][k] = new Button("0");
                 mapsButtons[i][k].setPrefHeight(20);
                 mapsButtons[i][k].setPrefWidth(20);
                 //mapsButtons[i][k].setVisible(false);
-                mapsButtons[i][k].setId(i+","+k);
+                mapsButtons[i][k].setId(i + "," + k);
                 mapsButtons[i][k].setStyle("-fx-background-color: #fff;");//ustawienie koloru tła przycisku
                 mapsButtons[i][k].setOnAction(new EventHandler<ActionEvent>() {
                     @Override
                     public void handle(ActionEvent event) {
                         //wyświetlenie współrzędnych przycisku, jego ID
-                        System.out.println(((Button)event.getSource()).getId().toString());
+                        coordinateLabel.setText("Kliknięto w pole " + ((Button) event.getSource()).getId().toString());
+                        System.out.println(((Button) event.getSource()).getId().toString());
                     }
                 });
                 gridPaneMap.add(mapsButtons[i][k], i, k);//dodanie do grid panelu przycisku
@@ -79,9 +193,7 @@ public class Main extends Application {
 
 
         Scene scene = new Scene(gridPane);//tworzenie sceny z GridPane
-
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));//ładowanie root z lokacją
-
         primaryStage.setTitle("Project PO JAVAFX");
         primaryStage.setScene(scene);//ładowanie z GridPane
 //        primaryStage.setScene(new Scene(root, 900, 750));//ładowanie z root
@@ -98,6 +210,21 @@ public class Main extends Application {
 //        primaryStage.setX(0);
 //        primaryStage.setY(0);//ustawiają domyślne miejsca przymocowania aplikacji
 
+
+//***********KOD PROGRAMU ****************************
+        Object guardian = new Object();
+        setRestaurantPosition();
+
+        Random random = new Random(System.nanoTime());
+
+        LinkedList<Deliverer> deliverers_list = new LinkedList<>();
+        LinkedList<Meal> meals_list = new LinkedList<>();
+
+        RandomGenerator randomGenerator = new RandomGenerator();
+
+        Map map = new Map(width, lenght, wRestaurant, lRestaurant);
+        map.setMapGUI(mapsButtons);
+
         primaryStage.show();
     }
 
@@ -105,6 +232,18 @@ public class Main extends Application {
 //
 //        return gridPane;
 //    }
+
+
+    /**
+     * Metoda wywoływana automatycznie na koniec działania programu. Na kliknięcie przycisku X.
+     *
+     * @throws Exception musi być
+     */
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        System.out.println("Koniec");
+    }
 
     public static void main(String[] args) {
         launch(args);
--- src/skeletor/Map.java
+++ src/skeletor/Map.java
@@ -1,6 +1,8 @@
 package skeletor;
 
 import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import packetGUI.Main;
 import skeletor.Person.Client;
 
 import java.util.LinkedList;
@@ -16,8 +18,9 @@ public class Map {
 
     /**
      * Konstruktor mapy, wraz z ustawieniem pozycji restauracji
-     * @param width wysokość mapy
-     * @param lenght szerogość mapy
+     *
+     * @param width       wysokość mapy
+     * @param lenght      szerogość mapy
      * @param wRestaurant współrzędna X restauracji
      * @param lRestaurant współrzędna Y restauracji
      */
@@ -28,28 +31,49 @@ public class Map {
         map[wRestaurant][lRestaurant] = 1;
     }
 
+    public void setMapGUI(Button[][] mapGUI) {
+        this.mapGUI = mapGUI;
+        setRestaurantOnMap();
+    }
+
+    private void setRestaurantOnMap() {
+        String restaurantCoordinate = Main.getwRestaurant() + "," + Main.getlRestaurant();
+        set_restaurant:
+        for (int i = 0; i < Main.getWidth(); i++) {
+            for (int k = 0; k < Main.getLenght(); k++) {
+                if (mapGUI[i][k].getId().equals(restaurantCoordinate)) {
+                    mapGUI[i][k].setTextFill(Color.WHITE);//ustawienie koloru wypełnienia
+                    mapGUI[i][k].setText("R");//ustawienie etykiety przycisku
+                    mapGUI[i][k].setStyle("-fx-background-color: #990000;");//ustawienie czerwonego tła przycisku
+                    break set_restaurant;//przerwanie, przecież jest tylko jedna restauracja
+                }
+            }
+        }
+    }
+
     /**
      * Metoda dodaje klientów na mapie.
+     *
      * @param clients lista klientów
      */
-    public void addClientToMap(LinkedList<Client> clients){
-        for (Client x: clients) {
+    public void addClientToMap(LinkedList<Client> clients) {
+        for (Client x : clients) {
             String address = x.getAddress();
             String[] coordinats = address.split(":");
             int width = Integer.parseInt(coordinats[0]);
             int lenght = Integer.parseInt(coordinats[1]);
             map[width][lenght] = 2;
-            if (width-1 >= 0){
-                map[width-1][lenght] = 0;
+            if (width - 1 >= 0) {
+                map[width - 1][lenght] = 0;
             }
-            if (width+1 < map.length){
-                map[width+1][lenght] = 0;
+            if (width + 1 < map.length) {
+                map[width + 1][lenght] = 0;
             }
-            if (lenght-1 >= 0){
-                map[width][lenght-1] = 0;
+            if (lenght - 1 >= 0) {
+                map[width][lenght - 1] = 0;
             }
-            if (lenght+1 < map[width].length){
-                map[width][lenght+1] = 0;
+            if (lenght + 1 < map[width].length) {
+                map[width][lenght + 1] = 0;
             }
             displayMap();
         }
@@ -59,14 +83,15 @@ public class Map {
      * Metoda ustawia na mapie znacznik dostawcy. Wykonywać tę metodę może maksymalnie
      * jeden dostawca w tym samym czasie. Stare współrzędne w macierzy w przypadku
      * możliwości wykonania ruchu są ustawiane na 0;
-     * @param oldX stary rząd położenia dostawcy
-     * @param oldY stara kolumna położenia dostawcy
-     * @param posX nowy rząd położenia dostawcy
-     * @param posY nowa kolumna położenia dostawcy
+     *
+     * @param oldX     stary rząd położenia dostawcy
+     * @param oldY     stara kolumna położenia dostawcy
+     * @param posX     nowy rząd położenia dostawcy
+     * @param posY     nowa kolumna położenia dostawcy
      * @param guardian obiekt strażnika pilnującego synchronizacji
      * @return true - dostawca może się poruszyć na wskazane pole, false - jeśli pole docelowe jest zajęte
      */
-    public boolean setDelivererPositionOnMap(int oldX, int oldY, int posX, int posY, Object guardian){
+    public boolean setDelivererPositionOnMap(int oldX, int oldY, int posX, int posY, Object guardian) {
         synchronized (guardian) {
             if (map[posX][posY] == 0) {
                 if (map[oldX][oldY] != 1 && map[oldX][oldY] != 2) {
@@ -76,13 +101,13 @@ public class Map {
                 displayMap();
                 return true;
             } else if (map[posX][posY] == 2) {
-                if (oldX < posX){
+                if (oldX < posX) {
                     posX--;
-                }else if (oldX > posX){
+                } else if (oldX > posX) {
                     posX++;
-                }else if (oldY < posY){
+                } else if (oldY < posY) {
                     posY--;
-                }else if (oldY > posY){
+                } else if (oldY > posY) {
                     posY++;
                 }
                 if (map[oldX][oldY] != 1 && map[oldX][oldY] != 2) {
@@ -91,13 +116,13 @@ public class Map {
                 map[posX][posY] = 3;
                 displayMap();
                 return true;
-            }else if (map[posX][posY] == 1){
+            } else if (map[posX][posY] == 1) {
                 if (map[oldX][oldY] != 1 && map[oldX][oldY] != 2) {
                     map[oldX][oldY] = 0;
                     displayMap();
                 }
                 return true;
-            }else {
+            } else {
                 return false;
             }
         }
@@ -106,10 +131,10 @@ public class Map {
     /**
      * Wyświetlenie mapy.
      */
-    public void displayMap(){
-        for (int x[]: map){
-            for(int y: x){
-                System.out.print(y+" ");
+    public void displayMap() {
+        for (int x[] : map) {
+            for (int y : x) {
+                System.out.print(y + " ");
             }
             System.out.println();
         }
